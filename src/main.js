46e8f694c9c61fe4fe0a308d627bb29715be8755
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2024-01-30 18:20:01 +0200

    test

--- src/main.js
+++ src/main.js
@@ -27,110 +27,110 @@ function hideLoader() {
   loader.style.display = 'none';
 }
 
-// async function searchImages(query, currentPage) {
-//   query = searchQuery;
+async function searchImages(query, currentPage) {
+  query = searchQuery;
 
-//   const requestParams = {
-//     key: API_KEY,
-//     q: searchQuery,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//     page: currentPage,
-//     per_page: perPage,
-//   };
-//   const searchParams = new URLSearchParams(requestParams);
+  const requestParams = {
+    key: API_KEY,
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: currentPage,
+    per_page: perPage,
+  };
+  const searchParams = new URLSearchParams(requestParams);
 
-//   showLoader();
+  showLoader();
 
 
-// }
+}
   
 
 
 // эу функцию переделать и внутрь первой запихнуть
 // начинать с try { const response = await axios.get(
-function searchImages(query) {
-  requestParams.q = query;
+// function searchImages(query) {
+//   requestParams.q = query;
 
-  showLoader();
+//   showLoader();
 
-  fetch(`https://pixabay.com/api/?${searchParams}`)
-    .then(response => {
-      hideLoader();
+//   fetch(`https://pixabay.com/api/?${searchParams}`)
+//     .then(response => {
+//       hideLoader();
 
-      if (!response.ok) {
-        throw new Error(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-      }
-      return response.json();
-    })
+//       if (!response.ok) {
+//         throw new Error(
+//           'Sorry, there are no images matching your search query. Please try again!'
+//         );
+//       }
+//       return response.json();
+//     })
 
-    .then(({ hits }) => {
-      const gallery = document.querySelector('.gallery');
+//     .then(({ hits }) => {
+//       const gallery = document.querySelector('.gallery');
 
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionDelay: 250,
-        captionsData: 'alt',
-        close: true,
-      });
+//       const lightbox = new SimpleLightbox('.gallery a', {
+//         captionDelay: 250,
+//         captionsData: 'alt',
+//         close: true,
+//       });
 
-      gallery.innerHTML = '';
+//       gallery.innerHTML = '';
 
-      gallery.innerHTML = hits.reduce(
-        (html, image) =>
-          html +
-          `<a class="gallery-link" href="${image.largeImageURL}">
-            <img
-                class="gallery-image"
-                src="${image.webformatURL}"
-                alt="${image.tags}"
-            />
-           <ul class="info-list">
-              <li class="info-item">
-                  <p class="info-title">Likes</p>
-                  <p class="info-value">${image.likes}</p>
-              </li>
-              <li class="info-item">
-                  <p class="info-title">Views</p>
-                  <p class="info-value">${image.views}</p>
-              </li>
-              <li class="info-item">
-                  <p class="info-title">Comments</p>
-                  <p class="info-value">${image.comments}</p>
-              </li>
-              <li class="info-item">
-                  <p class="info-title">Downloads</p>
-                  <p class="info-value">${image.downloads}</p>
-              </li>
-            </ul>
-        </a>`,
-        ''
-      );
+//       gallery.innerHTML = hits.reduce(
+//         (html, image) =>
+//           html +
+//           `<a class="gallery-link" href="${image.largeImageURL}">
+//             <img
+//                 class="gallery-image"
+//                 src="${image.webformatURL}"
+//                 alt="${image.tags}"
+//             />
+//            <ul class="info-list">
+//               <li class="info-item">
+//                   <p class="info-title">Likes</p>
+//                   <p class="info-value">${image.likes}</p>
+//               </li>
+//               <li class="info-item">
+//                   <p class="info-title">Views</p>
+//                   <p class="info-value">${image.views}</p>
+//               </li>
+//               <li class="info-item">
+//                   <p class="info-title">Comments</p>
+//                   <p class="info-value">${image.comments}</p>
+//               </li>
+//               <li class="info-item">
+//                   <p class="info-title">Downloads</p>
+//                   <p class="info-value">${image.downloads}</p>
+//               </li>
+//             </ul>
+//         </a>`,
+//         ''
+//       );
 
-      lightbox.refresh();
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
-        message: error.message,
-        position: 'topRight',
-      });
-    });
-}
+//       lightbox.refresh();
+//     })
+//     .catch(error => {
+//       iziToast.error({
+//         title: 'Error',
+//         message: error.message,
+//         position: 'topRight',
+//       });
+//     });
+// }
 
 
 
-searchForm.addEventListener('submit', event => {
-  event.preventDefault();
+// searchForm.addEventListener('submit', event => {
+//   event.preventDefault();
 
-  const searchQuery = searchInput.value.trim();
-  currentPage = 1;
-  loadMoreButton.style.display = 'none';
-  searchImages(searchQuery, currentPage);
-  searchForm.reset();
-});
+//   const searchQuery = searchInput.value.trim();
+//   currentPage = 1;
+//   loadMoreButton.style.display = 'none';
+//   searchImages(searchQuery, currentPage);
+//   searchForm.reset();
+// });
 
 // LOAD MORE
 
